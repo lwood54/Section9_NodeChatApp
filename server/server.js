5f6609cd6aa517636451d7d3902cb8668783ff37
Logan Wood <lwood3499@gmail.com>
2017-07-20 19:17:13 -0500

    added connect and disconnect event handlers with socket.io

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
     // built-in Node method to clean up path names
 const path = require('path');
-    // instead of (__dirname + '/../public')
-const publicPath = path.join(__dirname, '../public');
+    // load http module
+const http = require('http');
     // create a new express application
 const express = require('express');
+    // instead of (__dirname + '/../public')
+const publicPath = path.join(__dirname, '../public');
+    // get 'socket.io' resources
+const socketIO = require('socket.io');
+
+
     // create environment variable to store PORT options
 const port = process.env.PORT || 3000;
-
     // configure the express static middleware
 var app = express();
+    // create a variable that helps us create a server with http
+var server = http.createServer(app);
+    // set up the use of socket.io
+var io = socketIO(server);
+    // Use Express to connect our static/public folders and serve them up to browser
 app.use(express.static(publicPath));
 
+// lets you listen for a 'connection' from the client
+io.on('connection', function(socket) {
+    console.log('New user connected');
+
+    socket.on('disconnect', function() {
+        console.log('User was disconnected');
+    });
+});
     // set up listen and serve of port
-app.listen(port, function() {
+server.listen(port, function() {
     console.log(`Started up at port ${port}`);
 });
